30a910689d7efbed810586e1d634fceee23afe1c
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-01-09 23:04:43 +0530

    Connected to MongoDb using Mongoose

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const date = require(__dirname + '/date.js');
 
 const app = express();
@@ -8,8 +9,39 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-let items = [];
-let workItems = [];
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+
+const Item = mongoose.model('item', itemsSchema);
+
+const item1 = new Item({
+    name: 'Welcome to your todolist!'
+});
+
+const item2 = new Item({
+    name: 'Second Task'
+});
+
+const item3 = new Item({    
+    name: 'Third Task'
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems , (err) =>{
+    if(err){
+        console.log(err);
+    } else {
+        console.log('Successfully saved default items to DB');
+    }
+});
+
+// let items = [];
+// let workItems = [];
 
 app.get('/', (req, res) => {
 
